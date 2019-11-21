84455768ecd42abc21c328feb1afd0acaf07c819
mjg6452 <57972444+mjg6452@users.noreply.github.com>
2019-11-21 13:33:15 -0600

    Started Python, Trial Data

---
+++ bioinformaticsFinal.py
@@ -0,0 +1,47 @@
+import random
+
+populationSize=100
+repeater = 'CGG'
+standardRepetitions = 30
+
+#FMR1 30 CGG
+#ATXN1 33 CAG
+#RUNX2 22 CAG + 14 GCG
+
+population=[]
+tracker=[]
+for i in range(populationSize):
+    gene=''
+    for i in range(standardRepetitions):
+        gene=gene+repeater
+    population.append(gene)
+    tracker.append([])
+
+def repetitions(gene):
+    repetitions=0
+    current=0
+    while(gene[current:current+3]==repeater):
+        repetitions+=1
+        current+=3
+    return repetitions
+
+def generation(population,tracker):
+    for i in range(len(population)):
+        changeReps=random.random()
+        if(changeReps<0.05):
+            population[i]=population[i]+repeater
+        elif(changeReps<0.1):
+            population[i]=population[i][:-len(repeater)]
+        tracker[i].append(repetitions(population[i]))
+    return population,tracker
+
+for i in range(100):
+    population,tracker=generation(population,tracker)
+
+for i in population:
+    print(repetitions(i))
+
+for i in tracker:
+    for j in i:
+        print(j,end=',')
+    print()
